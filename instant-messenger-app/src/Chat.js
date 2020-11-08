c7d50202733205dd7c3255eb57226dc93a1e4858
Mark Rogers <mark_199518@hotmail.co.uk>
2020-11-08 16:14:03 +0000

    Post message

--- instant-messenger-app/src/Chat.js
+++ instant-messenger-app/src/Chat.js
@@ -2,10 +2,26 @@ import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, MoreVert, SearchOutlined} from "@material-ui/icons";
 import InsertEmoticon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "./axios.js";
 import './Chat.css';
 
 function Chat({messages}) {
+
+    const [input, setInput] = useState("");
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await axios.post('/messages/new', {
+            "message": input,
+            "name": "Demo App",
+            "timestamp": "Demo timestamp",
+            "received": false,
+        });
+
+        setInput('');
+    };
+
     return (
         <div className='chat'>
             <div className="chat-header">
@@ -44,8 +60,10 @@ function Chat({messages}) {
             <div className="chat-footer">
                 <InsertEmoticon/>  
                 <form>
-                    <input placeholder="Type a message .. " type="text"/>
-                    <button type="submit">Send a Message</button>
+                    <input value={input} 
+                           onChange = {(e) => setInput(e.target.value)} 
+                           placeholder="Type a message .. " type="text"/>
+                    <button onClick={sendMessage} type="submit">Send a Message</button>
                 </form>
                 <MicIcon/>
             </div>            
